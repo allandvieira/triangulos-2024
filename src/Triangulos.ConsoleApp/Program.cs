6c720b345d83ea212a04964feb57bf774d2412cd
Allan <Allan@DESKTOP-RA9V1AJ>
2024-04-09 13:36:12 -0300

    Alterações Código Triângulo

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insira o valor de X:");
+            double X = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor de Y:");
+            double Y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor de Z:");
+            double Z = double.Parse(Console.ReadLine());
+
+            Triangulo triangulo = new Triangulo(X, Y, Z);
+
+            if (triangulo.eValido())
+            {
+                Console.WriteLine(triangulo.TipoTriangulo());
+            }
+            else
+            {
+                Console.WriteLine("Os valores fornecidos não formam um triângulo.");
+            }
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos.ConsoleApp
+{
+    internal class Triangulo
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Triangulo(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public bool eValido()
+        {
+            if (X < Y + Z && Y < X + Z && Z < X + Y)
+                return true;
+            else
+                return false;
+        }
+
+        public string TipoTriangulo()
+        {
+            if (!eValido())
+                return "Os valores fornecidos não formam um triângulo.";
+            else if (X == Y && Y == Z)
+                return "O triângulo é equilátero.";
+            else if (X == Y || Y == Z || Z == X)
+                return "O triângulo é isósceles.";
+            else
+                return "O triângulo é escaleno.";
+        }
+    }
+}
